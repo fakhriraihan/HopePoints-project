7fd76b48fdb40876dc74fef5e69b164ba3597334
daffarizky16 <123257354+daffarizky16@users.noreply.github.com>
2023-05-29 21:31:49 +0700

    Update About.js

--- src/Components/AboutComp/About.js
+++ src/Components/AboutComp/About.js
@@ -1,21 +1,43 @@
+import React from 'react';
+// import { Container } from 'react-bootstrap';
+// import { useNavigate } from 'react-router-dom';
+import './about.css';
+import BasicExample from './card';
+// import AboutPage from '../../Pages/AboutPage/AboutPage';
+
 const About = () => {
   return (
     <>
-      <h1>ABOT LUUUURRRR!!!!!</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quos
-        corrupti quis blanditiis aut ab illo, nisi voluptates minus tenetur quia
-        ratione non error odit numquam perferendis quasi molestias velit
-        pariatur dolores dolore? Non ullam, minima, earum perspiciatis
-        cupiditate tenetur quas nostrum labore dolores doloremque fugiat placeat
-        eos consequuntur quisquam ipsa, veritatis error. Recusandae ullam
-        reprehenderit voluptates deleniti eos tenetur, maiores, ad neque
-        pariatur id possimus placeat assumenda quaerat voluptatum necessitatibus
-        amet laborum asperiores modi nemo itaque quibusdam, harum nam!
-        Cupiditate veniam, est eveniet repudiandae illo distinctio! Eaque quod
-        magni quae, ullam, maiores velit repellat fugit nisi, aperiam eum
-        assumenda?
-      </p>
+      <section className="about">
+        <div className="about-hopePoints">
+          <h1 className="title"> Tentang HopePoints</h1>
+          <p className="description">HopePoints adalah website pelayanan terhadap perempuan dan anak yang mendukung hak-hak perempuan dan anak agar terbebas dari segala bentuk kekerasan</p>
+        </div>
+        <div className="image">
+          <img src="/assets/logo.png" alt="hero HopePoints" />
+        </div>
+      </section>
+      <section className="aboutUs">
+        <h1 className="about-team"> Tentang Kami</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quos corrupti quis blanditiis aut ab illo, nisi voluptates minus tenetur quia ratione non error odit numquam perferendis quasi molestias velit pariatur dolores
+          dolore?
+        </p>
+        <div className="row justify-content-center">
+          <div className="col">
+            <BasicExample />
+          </div>
+          <div className="col">
+            <BasicExample />
+          </div>
+          <div className="col">
+            <BasicExample />
+          </div>
+          <div className="col">
+            <BasicExample />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
