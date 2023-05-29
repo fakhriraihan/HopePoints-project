5ac417daaf0fdd0819e56e133f8c78a5b430a047
fakhriraihan <fakhri.raihan10@gmail.com>
2023-05-29 22:21:31 +0700

    edit about and form css

--- src/Components/AboutComp/About.js
+++ src/Components/AboutComp/About.js
@@ -8,32 +8,38 @@ import BasicExample from './card';
 const About = () => {
   return (
     <>
-      <section className="about">
-        <div className="about-hopePoints">
-          <h1 className="title"> Tentang HopePoints</h1>
-          <p className="description">HopePoints adalah website pelayanan terhadap perempuan dan anak yang mendukung hak-hak perempuan dan anak agar terbebas dari segala bentuk kekerasan</p>
+      <section className='about'>
+        <div className='about-hopePoints'>
+          <h1 className='title'> Tentang HopePoints</h1>
+          <p className='description'>
+            HopePoints adalah website pelayanan terhadap perempuan dan anak yang
+            mendukung hak-hak perempuan dan anak agar terbebas dari segala
+            bentuk kekerasan
+          </p>
         </div>
-        <div className="image">
-          <img src="/assets/logo.png" alt="hero HopePoints" />
+        <div className='image'>
+          <img src='/assets/logo.png' alt='hero HopePoints' />
         </div>
       </section>
-      <section className="aboutUs">
-        <h1 className="about-team"> Tentang Kami</h1>
+      <section className='aboutUs'>
+        <h1 className='about-team'> Tentang Kami</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quos corrupti quis blanditiis aut ab illo, nisi voluptates minus tenetur quia ratione non error odit numquam perferendis quasi molestias velit pariatur dolores
-          dolore?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quos
+          corrupti quis blanditiis aut ab illo, nisi voluptates minus tenetur
+          quia ratione non error odit numquam perferendis quasi molestias velit
+          pariatur dolores dolore?
         </p>
-        <div className="row justify-content-center">
-          <div className="col">
+        <div className='card-team'>
+          <div className='col'>
             <BasicExample />
           </div>
-          <div className="col">
+          <div className='col'>
             <BasicExample />
           </div>
-          <div className="col">
+          <div className='col'>
             <BasicExample />
           </div>
-          <div className="col">
+          <div className='col'>
             <BasicExample />
           </div>
         </div>
