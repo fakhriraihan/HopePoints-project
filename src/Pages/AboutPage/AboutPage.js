73912b0bb9fc31b7ef87987bcb0801e745dcefde
daffarizky16 <123257354+daffarizky16@users.noreply.github.com>
2023-05-29 21:25:09 +0700

    Update AboutPage.js
    about page layout

--- src/Pages/AboutPage/AboutPage.js
+++ src/Pages/AboutPage/AboutPage.js
@@ -1,28 +1,46 @@
 import React from 'react';
-import Navigation from '../../Components/Navigation/Navigation';
+// import { Container } from 'react-bootstrap';
+// import { useNavigate } from 'react-router-dom';
+import './about.css';
+import BasicExample from './card';
+// import AboutPage from '../../Pages/AboutPage/AboutPage';
 
-const AboutPage = () => {
+const About = () => {
   return (
     <>
-      <Navigation />
-      <>
+      <section className="about">
+        <div className="about-hopePoints">
+          <h1 className="title"> Tentang HopePoints</h1>
+          <p className="description">HopePoints adalah website pelayanan terhadap perempuan dan anak yang mendukung hak-hak perempuan dan anak agar terbebas dari segala bentuk kekerasan</p>
+        </div>
+        <div className="image">
+          <img src="/assets/logo.png" alt="hero HopePoints" />
+        </div>
+      </section>
+      <section className="aboutUs">
+        <h1 className="about-team"> Tentang Kami</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
-          asperiores autem explicabo ea expedita deleniti inventore, quidem
-          reprehenderit repellendus illo incidunt, impedit magni molestiae
-          velit! Dolorum numquam dolore aut quas molestias ea deleniti mollitia
-          nulla officia fugiat. Perspiciatis enim numquam, quas debitis eum esse
-          deserunt quo distinctio quibusdam! Est aspernatur unde eligendi
-          doloremque voluptatem ratione repellendus, facere saepe, tempore,
-          minima rerum quo molestiae odit neque voluptatum assumenda tempora
-          voluptate vitae alias ullam aliquam maiores ducimus porro. Sequi nobis
-          voluptas, autem quibusdam nulla rerum inventore consectetur corporis
-          fugiat harum maxime mollitia labore distinctio incidunt at illo iure
-          ea, tempora neque in.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quos corrupti quis blanditiis aut ab illo, nisi voluptates minus tenetur quia ratione non error odit numquam perferendis quasi molestias velit pariatur dolores
+          dolore?
         </p>
-      </>
+        <div className="row justify-content-center">
+          <div className="col">
+            <BasicExample />
+          </div>
+          <div className="col">
+            <BasicExample />
+          </div>
+          <div className="col">
+            <BasicExample />
+          </div>
+          <div className="col">
+            <BasicExample />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
 
-export default AboutPage;
+export default About;
+
