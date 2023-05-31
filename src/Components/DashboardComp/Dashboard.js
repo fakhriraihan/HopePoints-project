0a0bb4248a8af514fe1281486f5381bdd8c7f41c
fakhriraihan <fakhri.raihan10@gmail.com>
2023-05-31 23:06:20 +0700

    show marker on dashboard map

--- src/Components/DashboardComp/Dashboard.js
+++ src/Components/DashboardComp/Dashboard.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import Nav from '../../Components/DashboardComp/Nav';
+import Map, {
+  Marker,
+  NavigationControl,
+  ScaleControl,
+  GeolocateControl,
+} from 'react-map-gl';
+import { useState, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../Config/firebase';
 import { Card } from 'react-bootstrap';
 import './dashboardcomp.css';
 
+const token = process.env.REACT_APP_MAPBOX_TOKEN;
 const Dashboard = ({ Toggle }) => {
+  const [reports, setReports] = useState([]);
+  const [viewport, setViewPort] = useState({
+    longitude: 117.27756850787405,
+    latitude: 0.09273370918533735,
+    zoom: 4.3,
+  });
+
+  useEffect(() => {
+    const getReports = async () => {
+      try {
+        const reportsCollection = collection(db, 'reports');
+        const reportsSnapshot = await getDocs(reportsCollection);
+        const reportsData = reportsSnapshot.docs.map((doc) => doc.data());
+        setReports(reportsData);
+      } catch (error) {
+        console.error('Error getting reports: ', error);
+      }
+    };
+
+    getReports();
+  }, []);
+
   return (
     <div className='px-3'>
       <Nav Toggle={Toggle} />
@@ -36,7 +68,40 @@ const Dashboard = ({ Toggle }) => {
           </div>
         </div>
         <Card className='my-3' style={{ width: '100%', height: '28rem' }}>
-          <Card.Body>Ini untuk maps</Card.Body>
+          <Card.Body>
+            <Map
+              initialViewState={viewport}
+              mapboxAccessToken={token}
+              mapStyle='mapbox://styles/renanda26/cli49zhib02nc01qyaka1dq8w'
+              width='100%'
+              height='100%'
+              onViewportChange={setViewPort}
+            >
+              {reports.map((report) => (
+                <Marker
+                  key={report.idReport}
+                  latitude={report.location.latitude}
+                  longitude={report.location.longitude}
+                  offsetleft={-3.5 * viewport.zoom}
+                  offsetTop={-7 * viewport.zoom}
+                  draggable={false}
+                  style={{ zIndex: 999 }}
+                >
+                  <i
+                    className='fa-solid fa-location-dot'
+                    style={{
+                      fontSize: 7 * viewport.zoom,
+                      color: 'tomato',
+                      cursor: 'pointer',
+                    }}
+                  ></i>
+                </Marker>
+              ))}
+              <GeolocateControl position='bottom-right' />
+              <NavigationControl position='bottom-right' />
+              <ScaleControl />
+            </Map>
+          </Card.Body>
         </Card>
       </div>
     </div>
--- src/Components/MapComp/Maps.js
+++ src/Components/MapComp/Maps.js
@@ -1,74 +1,71 @@
-import React, { useState, useEffect } from "react";
-import MapGL, { Marker, Popup, NavigationControl, ScaleControl, GeolocateControl } from "react-map-gl";
+import React, { useState, useEffect } from 'react';
+import Map, {
+  Marker,
+  Popup,
+  NavigationControl,
+  ScaleControl,
+  GeolocateControl,
+} from 'react-map-gl';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../Config/firebase';
 
 const token = process.env.REACT_APP_MAPBOX_TOKEN;
 const MapComponent = () => {
-  const [newPlace, setNewPlace] = useState(null); // [longitude, latitude
-  const [showPopup, setShowPopup] = React.useState(true);
+  const [reports, setReports] = useState([]);
   const [viewport, setViewPort] = useState({
     longitude: 117.27756850787405,
     latitude: 0.09273370918533735,
     zoom: 4.3,
   });
 
-  const handleMarkerDragEnd = (event) => {
-    const { lngLat } = event;
-    const { lng, lat } = lngLat;
-    setNewPlace({ lat, long: lng });
-  };
-
   useEffect(() => {
-    // Set initial marker on map load
-    setNewPlace({
-      lat: viewport.latitude,
-      long: viewport.longitude,
-    });
+    const getReports = async () => {
+      try {
+        const reportsCollection = collection(db, 'reports');
+        const reportsSnapshot = await getDocs(reportsCollection);
+        const reportsData = reportsSnapshot.docs.map((doc) => doc.data());
+        setReports(reportsData);
+      } catch (error) {
+        console.error('Error getting reports: ', error);
+      }
+    };
+
+    getReports();
   }, []);
 
-  const handleGeolocateClick = () => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setViewPort((prevViewport) => ({
-          ...prevViewport,
-          longitude,
-          latitude,
-        }));
-        setNewPlace({ lat: latitude, long: longitude });
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  };
-
-  useEffect(() => {
-    if (newPlace) {
-      console.log(newPlace);
-    }
-  }, [newPlace]);
-
   return (
-    <div className="map-comp" style={{ width: "100vw", height: "100vh" }}>
-      <MapGL initialViewState={viewport} mapboxAccessToken={token} mapStyle="mapbox://styles/renanda26/cli49zhib02nc01qyaka1dq8w" width="100%" height="100%" onViewportChange={setViewPort}>
-        {newPlace && (
-          <>
-            <Marker latitude={newPlace?.lat} longitude={newPlace?.long} offsetleft={-3.5 * viewport.zoom} offsetTop={-7 * viewport.zoom} draggable={true} onDragEnd={handleMarkerDragEnd} style={{ zIndex: 999 }}>
-              <i
-                className="fa-solid fa-location-dot"
-                style={{
-                  fontSize: 7 * viewport.zoom,
-                  color: "tomato",
-                  cursor: "pointer",
-                }}
-              ></i>
-            </Marker>
-          </>
-        )}
-        <GeolocateControl position="bottom-right" onGeolocate={handleGeolocateClick} />
-        <NavigationControl position="bottom-right" />
-        <ScaleControl />
-        {/* <Popup
+    <Map
+      initialViewState={viewport}
+      mapboxAccessToken={token}
+      mapStyle='mapbox://styles/renanda26/cli49zhib02nc01qyaka1dq8w'
+      width='100%'
+      height='100%'
+      onViewportChange={setViewPort}
+    >
+      {reports.map((report) => (
+        <Marker
+          key={report.idReport}
+          latitude={report.location.latitude}
+          longitude={report.location.longitude}
+          offsetleft={-3.5 * viewport.zoom}
+          offsetTop={-7 * viewport.zoom}
+          draggable={false}
+          style={{ zIndex: 999 }}
+        >
+          <i
+            className='fa-solid fa-location-dot'
+            style={{
+              fontSize: 7 * viewport.zoom,
+              color: 'tomato',
+              cursor: 'pointer',
+            }}
+          ></i>
+        </Marker>
+      ))}
+      <GeolocateControl position='bottom-right' />
+      <NavigationControl position='bottom-right' />
+      <ScaleControl />
+      {/* <Popup
           longitude={113.764686933965}
           latitude={0.6763165071501142}
           anchor='bottom'
@@ -76,8 +73,7 @@ const MapComponent = () => {
         >
           You are here
         </Popup> */}
-      </MapGL>
-    </div>
+    </Map>
   );
 };
 export default MapComponent;
