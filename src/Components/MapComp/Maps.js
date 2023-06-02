3949c80e3961cdea95ce0b23591f63ddc9b83d2b
Ramji <ramjirenanda02@gmail.com>
2023-06-02 10:49:29 +0700

    Menambahkan marker lokasi data dinas

--- src/Components/MapComp/Maps.js
+++ src/Components/MapComp/Maps.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import Map, {
-  NavigationControl,
-  ScaleControl,
-  GeolocateControl,
-  Source,
-  Layer,
-} from 'react-map-gl';
+import Map, { NavigationControl, ScaleControl, GeolocateControl, Source, Layer, Marker } from 'react-map-gl';
 import { GetReport } from '../../Utils/crudData';
+import * as firestore from 'firebase/firestore';
+import { db } from '../../Config/firebase';
 
 const token = process.env.REACT_APP_MAPBOX_TOKEN;
 const MapComponent = () => {
   const [reports, setReports] = useState([]);
+  const [users, setUsers] = useState([]);
   const [viewport, setViewPort] = useState({
     longitude: 117.27756850787405,
     latitude: 0.09273370918533735,
@@ -27,20 +24,31 @@ const MapComponent = () => {
     })),
   };
 
+  useEffect(() => {
+    const unsubscribe = firestore.onSnapshot(
+      firestore.query(firestore.collection(db, 'users'), firestore.where('role', '==', 'office')),
+      (snapshot) => {
+        const userList = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        console.log('Retrieved data:', userList);
+        setUsers(userList);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
-    <Map
-      initialViewState={viewport}
-      mapboxAccessToken={token}
-      mapStyle='mapbox://styles/renanda26/cli49zhib02nc01qyaka1dq8w'
-      width='100%'
-      height='100%'
-      onViewportChange={setViewPort}
-    >
-      <Source id='heatmapData' type='geojson' data={heatmapData}>
+    <Map initialViewState={viewport} mapboxAccessToken={token} mapStyle="mapbox://styles/renanda26/cli49zhib02nc01qyaka1dq8w" width="100%" height="100%" onViewportChange={setViewPort}>
+      <Source id="heatmapData" type="geojson" data={heatmapData}>
         <Layer
-          id='heatmapLayer'
-          type='heatmap'
-          source='heatmapData'
+          id="heatmapLayer"
+          type="heatmap"
+          source="heatmapData"
           maxzoom={15}
           paint={{
             'heatmap-weight': {
@@ -57,21 +65,7 @@ const MapComponent = () => {
                 [15, 3],
               ],
             },
-            'heatmap-color': [
-              'interpolate',
-              ['linear'],
-              ['heatmap-density'],
-              0,
-              'rgba(236,222,239,0)',
-              0.2,
-              'rgb(208,209,230)',
-              0.4,
-              'yellow',
-              0.6,
-              'orange',
-              0.8,
-              'red',
-            ],
+            'heatmap-color': ['interpolate', ['linear'], ['heatmap-density'], 0, 'rgba(236,222,239,0)', 0.2, 'rgb(208,209,230)', 0.4, 'yellow', 0.6, 'orange', 0.8, 'red'],
             'heatmap-radius': {
               stops: [
                 [11, 15],
@@ -88,8 +82,20 @@ const MapComponent = () => {
           }}
         />
       </Source>
-      <GeolocateControl position='bottom-right' />
-      <NavigationControl position='bottom-right' />
+      {users.map((user) => (
+        <Marker key={user.id} latitude={user.location.latitude} longitude={user.location.longitude} offsetleft={-3.5 * viewport.zoom} offsetTop={-7 * viewport.zoom} draggable={false} style={{ zIndex: 999 }}>
+          <i
+            className="fa-solid fa-location-dot"
+            style={{
+              fontSize: 7 * viewport.zoom,
+              color: 'tomato',
+              cursor: 'pointer',
+            }}
+          ></i>
+        </Marker>
+      ))}
+      <GeolocateControl position="bottom-right" />
+      <NavigationControl position="bottom-right" />
       <ScaleControl />
       {/* <Popup
         longitude={113.764686933965}
