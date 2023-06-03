8853f50618a7680062668f9fb6e59b9724f25bc2
daffarizky16 <daffar315@gmail.com>
2023-06-03 19:14:49 +0700

    komponen card ini akan dihapus

--- src/Components/AboutComp/card.js
+++ src/Components/AboutComp/card.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './about.css';
@@ -17,23 +16,3 @@ const BasicExample = () => {
 };
 
 export default BasicExample;
-=======
-// import Button from 'react-bootstrap/Button';
-// import Card from 'react-bootstrap/Card';
-// import './about.css';
-
-// const BasicExample = () => {
-//   return (
-//     <Card style={{ width: '18rem' }}>
-//       <Card.Img variant="top" src="/assets/hero/hero.png" />
-//       <Card.Body>
-//         <Card.Title>Nama</Card.Title>
-//         <Card.Text>Front-End</Card.Text>
-//         <Button variant="primary">Connect</Button>
-//       </Card.Body>
-//     </Card>
-//   );
-// };
-
-// export default BasicExample;
->>>>>>> Stashed changes
