56f26528aabcff6a374e113991ff3eb3535aa2bf
daffarizky16 <daffar315@gmail.com>
2023-06-13 17:08:55 +0700

    penambahan konten home dan pembaruan about

--- src/Components/AboutComp/About.js
+++ src/Components/AboutComp/About.js
@@ -8,55 +8,55 @@ import daffa from '../../assets/team/daffa.jpg';
 const About = () => {
   return (
     <>
-      <section className='about'>
-        <div className='about-hopePoints'>
-          <h1 className='title'> Tentang HopePoints</h1>
-          <p className='description'>
-            HopePoints adalah website pelayanan terhadap perempuan dan anak yang
-            mendukung hak-hak perempuan dan anak agar terbebas dari segala
-            bentuk kekerasan
-          </p>
-        </div>
-        <div className='image'>
-          <img src={logo} alt='hero HopePoints' />
+      <section className="about">
+        <div className="d-flex">
+          <div className="about-hopePoints">
+            <h1 className="title"> Tentang HopePoints</h1>
+            <p className="description">
+              HopePoints adalah website pelayanan terhadap perempuan dan anak yang mendukung hak-hak perempuan dan anak agar terbebas dari segala bentuk kekerasan, dengan komitmen untuk membangun kesadaran dan menciptakan perubahan sosial
+              untuk mencapai dampak yang positif.
+            </p>
+          </div>
+          <div className="image">
+            <img src={logo} alt="hero HopePoints" />
+          </div>
         </div>
       </section>
-      <section className='aboutUs'>
-        <h1 className='about-team'> Tentang Kami</h1>
+      <section className="aboutUs">
+        <h1 className="about-team"> Tentang Kami</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quos
-          corrupti quis blanditiis aut ab illo, nisi voluptates minus tenetur
-          quia ratione non error odit numquam perferendis quasi molestias velit
-          pariatur dolores dolore?
+          Kami adalah tim yang berkomitmen dalam menyediakan layanan terbaik bagi perempuan dan anak-anak. Dengan fokus pada mendukung hak-hak perempuan dan anak, kami bertujuan untuk memerangi segala bentuk kekerasan dan menciptakan
+          perubahan sosial yang positif.
         </p>
-        <div className='cardItem'>
-          <div className='row justify-content-center about-team'>
-            <Card className='cardItemTeam' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={hero} />
+
+        <div className="cardItem">
+          <div className="row justify-content-center about-team">
+            <Card className="cardItemTeam" style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={hero} />
               <Card.Body>
                 <Card.Title>Fakhri Raihan</Card.Title>
                 <Card.Text>Front-End</Card.Text>
                 <button>Connect</button>
               </Card.Body>
             </Card>
-            <Card className='cardItemTeam' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={hero} />
+            <Card className="cardItemTeam" style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={hero} />
               <Card.Body>
                 <Card.Title>Ramji Renanda Sitorus</Card.Title>
                 <Card.Text>Front-End</Card.Text>
                 <button>Connect</button>
               </Card.Body>
             </Card>
-            <Card className='cardItemTeam' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={hero} />
+            <Card className="cardItemTeam" style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={hero} />
               <Card.Body>
                 <Card.Title>Muhammad Taufik</Card.Title>
                 <Card.Text>Front-End</Card.Text>
                 <button>Connect</button>
               </Card.Body>
             </Card>
-            <Card className='cardItemTeam' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={daffa} />
+            <Card className="cardItemTeam" style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={daffa} />
               <Card.Body>
                 <Card.Title>Daffa Rizky</Card.Title>
                 <Card.Text>Front-End</Card.Text>
--- src/Components/HomeComp/Hero.js
+++ src/Components/HomeComp/Hero.js
@@ -3,184 +3,84 @@ import { Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import hero from '../../assets/hero/hero.png';
 import './home.css';
-// import Dropdown from 'react-bootstrap/Dropdown';
-// import DropdownButton from 'react-bootstrap/DropdownButton';
+import { AiOutlineForm } from 'react-icons/ai';
+import { BsPinMap } from 'react-icons/bs';
+import { MDBAccordion, MDBAccordionItem, MDBContainer } from 'mdb-react-ui-kit';
 
 const Hero = () => {
   const navigate = useNavigate();
 
   return (
     <>
-      <Container className='hero'>
-        <div className='d-flex'>
-          <div className='wrap-text'>
-            <h1 className='title'>HopePoints</h1>
-            <p className='subtitle'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
-              eius? Obcaecati perferendis voluptas temporibus. Eius neque qui
-              quam quidem alias.
-            </p>
+      <Container className="hero">
+        <div className="d-flex">
+          <div className="wrap-text">
+            <h1 className="title">HopePoints</h1>
+            <p className="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, eius? Obcaecati perferendis voluptas temporibus. Eius neque qui quam quidem alias.</p>
             <button onClick={() => navigate('/form')}>Laporkan!</button>
           </div>
-          <div className='image'>
-            <img src={hero} alt='hero HopePoints' />
+          <div className="image">
+            <img src={hero} alt="hero HopePoints" />
           </div>
         </div>
       </Container>
-      <section className='feature'>
-        <h1 className='fiturTitle'>Explore Our Feature</h1>
-        <div className='overlay'>
-          <div className='featureForm'>
-            <img src='/assets/logo.png' alt='fitur form'></img>
-            <h2 className='featureFormTitle'>Form</h2>
+      <section className="feature">
+        <h1 className="fiturTitle">Explore Our Feature</h1>
+        <div className="overlay">
+          <div className="featureForm" style={{ fontSize: '100px' }}>
+            <h2 className="featureFormTitle">Form</h2>
+            <AiOutlineForm />
+            <p>Kamu dapat menggunakan fitur Form ini untuk membantu kami dengan mudah mengumpulkan informasi yang dibutuhkan </p>
           </div>
-          <div className='featureForm'>
-            <img src='/assets/logo.png' alt='fitur form'></img>
-            <h2 className='featureFormTitle'>Maps</h2>
+          <div className="featureForm" style={{ fontSize: '100px' }}>
+            <h2 className="featureFormTitle">Maps</h2>
+            <BsPinMap />
+            <p>Fitur Map ini akan membantu kamu dalam melihat lokasi yang dibutuhkan</p>
           </div>
-          <div className='featureForm'>
-            <img src='/assets/logo.png' alt='fitur form'></img>
-            <h2 className='featureFormTitle'>Office</h2>
+          <div className="featureForm" style={{ fontSize: '100px' }}>
+            <h2 className="featureFormTitle">Office</h2>
+            <i className="fa-regular fa-building"></i>
+            <p>Kamu bisa melihat daftar Kantor terdekat yang terdaftar pada fitur ini </p>
           </div>
         </div>
         <button onClick={() => navigate('')}>Registrasi</button>
       </section>
-      <section className='faq'>
-        <div className='container'>
-          <div className='faq-title'>
+      <section className="faq">
+        <div className="container">
+          <div className="faq-title">
             <h2>Cara Penggunaan</h2>
-            <p>
-              Silahkan cek tutorial berikut agar kamu tidak salah langkah ya
-            </p>
+            <p>Silahkan cek tutorial berikut agar kamu tidak salah langkah ya</p>
           </div>
-          <ul className='faq-list'>
-            <li
-              data-aos='fade-up'
-              data-aos-delay='100'
-              className='aos-init aos-animate'
-            >
-              <a
-                data-toggle='collapse'
-                className='collapsed'
-                href='#faq1'
-                aria-expanded='false'
-              >
-                Non consectetur a erat nam at lectus urna duis?{' '}
-                <i className='fas fa-arrow-up'></i>
-              </a>
-              <div id='faq1' className='collapse' data-parent='.faq-list'>
-                <p>
-                  Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id
-                  volutpat lacus laoreet non curabitur gravida. Venenatis lectus
-                  magna fringilla urna porttitor rhoncus dolor purus non.
-                </p>
-              </div>
-            </li>
-
-            <li
-              data-aos='fade-up'
-              data-aos-delay='200'
-              className='aos-init aos-animate'
-            >
-              <a data-toggle='collapse' href='#faq2' className='collapsed'>
-                Feugiat scelerisque varius morbi enim nunc faucibus a
-                pellentesque? <i className='fas fa-arrow-up'></i>
-              </a>
-              <div id='faq2' className='collapse' data-parent='.faq-list'>
-                <p>
-                  Dolor sit amet consectetur adipiscing elit pellentesque
-                  habitant morbi. Id interdum velit laoreet id donec ultrices.
-                  Fringilla phasellus faucibus scelerisque eleifend donec
-                  pretium. Est pellentesque elit ullamcorper dignissim. Mauris
-                  ultrices eros in cursus turpis massa tincidunt dui.
-                </p>
-              </div>
-            </li>
-
-            <li
-              data-aos='fade-up'
-              data-aos-delay='300'
-              className='aos-init aos-animate'
-            >
-              <a data-toggle='collapse' href='#faq3' className='collapsed'>
-                Dolor sit amet consectetur adipiscing elit pellentesque habitant
-                morbi? <i className='fas fa-arrow-up'></i>
-              </a>
-              <div id='faq3' className='collapse' data-parent='.faq-list'>
-                <p>
-                  Eleifend mi in nulla posuere sollicitudin aliquam ultrices
-                  sagittis orci. Faucibus pulvinar elementum integer enim. Sem
-                  nulla pharetra diam sit amet nisl suscipit. Rutrum tellus
-                  pellentesque eu tincidunt. Lectus urna duis convallis
-                  convallis tellus. Urna molestie at elementum eu facilisis sed
-                  odio morbi quis
-                </p>
-              </div>
-            </li>
-
-            <li
-              data-aos='fade-up'
-              data-aos-delay='400'
-              className='aos-init aos-animate'
-            >
-              <a data-toggle='collapse' href='#faq4' className='collapsed'>
-                Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?{' '}
-                <i className='fas fa-arrow-up'></i>
-              </a>
-              <div id='faq4' className='collapse' data-parent='.faq-list'>
-                <p>
-                  Dolor sit amet consectetur adipiscing elit pellentesque
-                  habitant morbi. Id interdum velit laoreet id donec ultrices.
-                  Fringilla phasellus faucibus scelerisque eleifend donec
-                  pretium. Est pellentesque elit ullamcorper dignissim. Mauris
-                  ultrices eros in cursus turpis massa tincidunt dui.
-                </p>
-              </div>
-            </li>
-
-            <li
-              data-aos='fade-up'
-              data-aos-delay='500'
-              className='aos-init aos-animate'
-            >
-              <a data-toggle='collapse' href='#faq5' className='collapsed'>
-                Tempus quam pellentesque nec nam aliquam sem et tortor
-                consequat? <i className='fas fa-arrow-up'></i>
-              </a>
-              <div id='faq5' className='collapse' data-parent='.faq-list'>
-                <p>
-                  Molestie a iaculis at erat pellentesque adipiscing commodo.
-                  Dignissim suspendisse in est ante in. Nunc vel risus commodo
-                  viverra maecenas accumsan. Sit amet nisl suscipit adipiscing
-                  bibendum est. Purus gravida quis blandit turpis cursus in
-                </p>
-              </div>
-            </li>
-
-            <li
-              data-aos='fade-up'
-              data-aos-delay='600'
-              className='aos-init aos-animate'
-            >
-              <a data-toggle='collapse' href='#faq6' className='collapsed'>
-                Tortor vitae purus faucibus ornare. Varius vel pharetra vel
-                turpis nunc eget lorem dolor?{' '}
-                <i className='fas fa-arrow-up'></i>
-              </a>
-              <div id='faq6' className='collapse' data-parent='.faq-list'>
-                <p>
-                  Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris
-                  vitae ultricies leo integer malesuada nunc vel. Tincidunt eget
-                  nullam non nisi est sit amet. Turpis nunc eget lorem dolor
-                  sed. Ut venenatis tellus in metus vulputate eu scelerisque.
-                  Pellentesque diam volutpat commodo sed egestas egestas
-                  fringilla phasellus faucibus. Nibh tellus molestie nunc non
-                  blandit massa enim nec.
-                </p>
-              </div>
-            </li>
-          </ul>
         </div>
+        <MDBContainer className="mt-1" style={{ maxWidth: '1200px' }}>
+          <MDBAccordion>
+            <MDBAccordionItem collapseId={1} headerTitle="Apa Itu HopePoints?" claassName="accordian-header">
+              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well
+              as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </MDBAccordionItem>
+            <MDBAccordionItem collapseId={2} headerTitle="Bagaimana Cara Melaporkan Kejadian?">
+              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as
+              well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>
+              , though the transition does limit overflow.
+            </MDBAccordionItem>
+            <MDBAccordionItem collapseId={3} headerTitle="Bagaimana Cara Melihat Laporan yang Telah Diproses?">
+              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well
+              as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </MDBAccordionItem>
+            <MDBAccordionItem collapseId={3} headerTitle="Bagaimana Cara Menyampaikan Feedback Terhadap Kantor Pelayanan?">
+              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well
+              as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </MDBAccordionItem>
+            <MDBAccordionItem collapseId={3} headerTitle="Bagaimana cara Menghubungi Admin?">
+              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well
+              as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </MDBAccordionItem>
+          </MDBAccordion>
+        </MDBContainer>
       </section>
     </>
   );
