1002fe68ee17ef3959ed4cd8b8f495e5ea9d11c9
opikzxx <taufikheryunanto@gmail.com>
2023-06-16 01:55:48 +0700

    update rating maps

--- src/Components/MapComp/Maps.js
+++ src/Components/MapComp/Maps.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Map, { NavigationControl, ScaleControl, GeolocateControl, Source, Layer, Marker, Popup } from 'react-map-gl';
 import { useNavigate } from 'react-router-dom';
 import { GetReport } from '../../Utils/crudData';
-import * as firestore from 'firebase/firestore';
+import { collection, getDocs, query, where, doc } from '@firebase/firestore';
 import { db } from '../../Config/firebase';
 import { Button } from 'react-bootstrap';
 import './popup.css';
@@ -14,9 +14,8 @@ const MapComponent = () => {
   const navigate = useNavigate();
   const [reports, setReports] = useState([]);
   const [users, setUsers] = useState([]);
+  const [reviews, setReviews] = useState([]);
   const [selectedMarker, setSelectedMarker] = useState(null);
-  const [currentValue, setCurrentValue] = useState(0);
-  const [hoverValue, setHoverValue] = useState(undefined);
   const stars = Array(5).fill(0);
   const [viewport, setViewPort] = useState({
     longitude: 117.27756850787405,
@@ -27,18 +26,6 @@ const MapComponent = () => {
   const colors = {
     orange: '#FFBA5A',
     grey: '#a9a9a9',
-  };
-
-  const handleClick = (value) => {
-    setCurrentValue(value);
-  };
-
-  const handleMouseOver = (newHoverValue) => {
-    setHoverValue(newHoverValue);
-  };
-
-  const handleMouseLeave = () => {
-    setHoverValue(undefined);
   };
 
   const heatmapData = {
@@ -52,27 +39,66 @@ const MapComponent = () => {
   };
 
   useEffect(() => {
-    const unsubscribe = firestore.onSnapshot(
-      firestore.query(firestore.collection(db, 'users'), firestore.where('role', '==', 'office')),
-      (snapshot) => {
-        const userList = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setUsers(userList);
-      },
-      (error) => {
-        console.log(error);
+    const loadUsersAndReviews = async () => {
+      try {
+        const usersCollectionRef = collection(db, 'users');
+        const querySnapshot = await getDocs(query(usersCollectionRef, where('role', '==', 'office')));
+  
+        if (!querySnapshot.empty) {
+          const usersData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+          setUsers(usersData);
+  
+          await loadReviewsByUsers(usersData);
+        } else {
+          console.log('No users with role "office" found!');
+        }
+      } catch (error) {
+        console.log('Error loading users and reviews:', error);
       }
-    );
-
-    return () => {
-      unsubscribe();
     };
+  
+    const loadReviewsByUsers = async (users) => {
+      try {
+        const reviewsData = [];
+  
+        for (const user of users) {
+          const userRef = doc(db, 'users', user.id);
+          const reviewsQuerySnapshot = await getDocs(collection(userRef, 'reviews'));
+          const userReviewsData = reviewsQuerySnapshot.docs.map((doc) => doc.data());
+          reviewsData.push(...userReviewsData);
+        }
+  
+        setReviews(reviewsData);
+      } catch (error) {
+        console.log('Error loading reviews:', error);
+      }
+    };
+  
+    loadUsersAndReviews();
   }, []);
 
+  const calculateRatingByIdOffice = (idOffice) => {
+    const ratingsByIdOffice = reviews
+      .filter((review) => review.idOffice === idOffice)
+      .map((review) => review.rating);
+  
+    const totalRating = ratingsByIdOffice.reduce((acc, rating) => acc + rating, 0);
+    const averageRating = totalRating / ratingsByIdOffice.length;
+  
+    return averageRating;
+  };
+              
+
   return (
-    <Map initialViewState={viewport} mapboxAccessToken={token} mapStyle="mapbox://styles/renanda26/cli49zhib02nc01qyaka1dq8w" width="100%" height="100%" onViewportChange={setViewPort}>
+    
+    <Map
+      initialViewState={viewport}
+      mapboxAccessToken={token}
+      mapStyle="mapbox://styles/renanda26/cli49zhib02nc01qyaka1dq8w"
+      width="100%"
+      height="100%"
+      onViewportChange={setViewPort}
+    >
       <Source id="heatmapData" type="geojson" data={heatmapData}>
         <Layer
           id="heatmapLayer"
@@ -129,46 +155,60 @@ const MapComponent = () => {
       </Source>
       {users.map((user) => (
         <React.Fragment key={user.id}>
-          <Marker latitude={user.location.latitude} longitude={user.location.longitude} offsetleft={-3.5 * viewport.zoom} offsetTop={-7 * viewport.zoom} draggable={false} style={{ zIndex: 1 }} onClick={() => setSelectedMarker(user)}>
+          <Marker
+            latitude={user.location.latitude}
+            longitude={user.location.longitude}
+            offsetLeft={-3.5 * viewport.zoom}
+            offsetTop={-7 * viewport.zoom}
+            draggable={false}
+            style={{ zIndex: 1 }}
+            onClick={() => setSelectedMarker(user)}
+          >
+
             <i
               className="fa-solid fa-building"
               style={{
-                fontSize: 5 * viewport.zoom,
+                fontSize: 4 * viewport.zoom,
                 color: '#f94892',
                 cursor: 'pointer',
               }}
             ></i>
           </Marker>
-          {selectedMarker !== null && selectedMarker.id === user.id && (
-            <Popup latitude={selectedMarker.location.latitude} longitude={selectedMarker.location.longitude} closeButton={true} closeOnClick={false} anchor="left" onClose={() => setSelectedMarker(null)} style={{ zIndex: 1 }}>
+          {selectedMarker !== null && selectedMarker.uid === user.uid && (
+            <Popup
+              latitude={selectedMarker.location.latitude}
+              longitude={selectedMarker.location.longitude}
+              closeButton={true}
+              closeOnClick={false}
+              anchor="left"
+              onClose={() => setSelectedMarker(null)}
+              style={{ zIndex: 1 }}
+            >
               <div className="popup-content">
                 {/* Konten popup */}
                 <h2>Informasi Dinas</h2>
-                <h4 className="nama-dinas">{user.name}</h4>
-                <p>Alamat: {user.address}</p>
-                <p>Phone: {user.phone}</p>
+                <h4 className="nama-dinas">{user?.name}</h4>
+                <p>Alamat: {user?.address}</p>
+                <p>Phone: {user?.phone}</p>
 
                 {/* Review */}
                 <div className="stars" style={styles.stars}>
-                  {stars.map((_, index) => {
-                    return (
-                      <FaStar
-                        key={index}
-                        size={24}
-                        onClick={() => handleClick(index + 1)}
-                        onMouseOver={() => handleMouseOver(index + 1)}
-                        onMouseLeave={handleMouseLeave}
-                        color={(hoverValue || currentValue) > index ? colors.orange : colors.grey}
-                        style={{
-                          marginRight: 10,
-                          cursor: 'pointer',
-                        }}
-                      />
-                    );
-                  })}
-                </div>
+                    {stars.map((_, index) => {
+                      return (
+                        <FaStar
+                          key={index}
+                          size={24}
+                          style={{
+                            marginRight: 2,
+                            cursor: 'pointer',
+                            color: calculateRatingByIdOffice(user.uid) > index ? colors.orange : colors.grey,
+                          }}
+                        />
+                      );
+                    })}
+                  </div>
 
-                <Button variant="pink" onClick={() => navigate(`/detailoffice/${user.id}`)}>
+                <Button variant="pink" onClick={() => navigate(`/detailoffice/${user.uid}`)}>
                   Detail Office
                 </Button>
               </div>
