2949045aa18fca1bcf94ca8a280e1db744cb651c
fakhriraihan <fakhri.raihan10@gmail.com>
2024-06-21 07:58:00 +0700

    update dashfooter comp

--- src/Components/DashboardComp/DashFooter.js
+++ src/Components/DashboardComp/DashFooter.js
@@ -3,9 +3,7 @@ import React from 'react';
 const DashFooter = () => {
   return (
     <div className='text-center text-white mt-3 py-3'>
-      <p>
-        Copyright &copy;2023 <span>HopePoints</span>. All rights reserved.
-      </p>
+      <p>HopePoints</p>
     </div>
   );
 };
